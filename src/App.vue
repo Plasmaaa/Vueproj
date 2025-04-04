bd3bea6290c61b487e4776c7d244945d88a89589
Plasmaaa <147718245+Plasmaaa@users.noreply.github.com>
2025-04-04 13:44:05 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -24,59 +24,10 @@ const selected = ref('')
 <template>
   <main>
     <div>
-    <h2>Formulaire</h2     >
-    
-    <form @submit.prevent>
-      <div>
-        <label for="texte">Texte : </label>
-        <input id="texte" type="text" v-model="texte" />
-      </div>
-
-      <div>
-        <label for="nombre">Nombre : </label>
-        <input id="nombre" type="number" v-model.number="nombre" />
-      </div>
-      <div class="result">
-      <h3>Moustaches :</h3>
-      <p>Nom : {{ nom }}</p>
-      <p>Nombre : {{ nombre }}</p>
+    <div>watchers
+      <input type="(ref)">
     </div>
- 
-    </form>
-    <div>
-      <input type="checkbox" id="checkbox" v-model="checked" />
-      <p v-if="checked"> la case est coché</p>
-    </div>
-    <div>Selected: {{ selected }}</div>
-
-    <div>
-<select v-model="selected">
-  <option disabled value="">Please select one</option>
-  <option>A</option>
-  <option>B</option>
-  <option>C</option>
-</select>
-
   </div>
-
-  
-
-    <button @click="increment">
-      ajouter
-    </button>
-    <button @click="decrement" :disabled="count ===0" >
-      enlever
-    </button>
-    <button @click="reset">
-      reset
-    </button>
-    <h1>
-      {{ count }} 
-    </h1>
-    
-  </div>
-
-    
   </main>
 </template>
 
